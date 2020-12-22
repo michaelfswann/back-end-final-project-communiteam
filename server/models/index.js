ce607f968a03eecfbe62742df1b137746b68622c
michaelfswann <mikeswann@mail.com>
2020-12-22 15:17:12 +0000

    POST req working for events

--- server/models/index.js
+++ server/models/index.js
@@ -4,4 +4,27 @@ async function getAllEvents() {
     const result = await query('SELECT * FROM event_table')
     return result.rows
 }
-module.exports = { getAllEvents }
+
+async function getEventById(id) {
+    const result = await query('SELECT * FROM event_table WHERE id = $1;', [id])
+    return result.rows
+}
+
+async function addEvent(event) {
+    const res = await query(
+        `INSERT INTO event_table (title, date, speaker, banner, description, numtickets, location) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING title;`,
+        [
+            event.title,
+            event.date,
+            event.speaker,
+            event.banner,
+            event.description,
+            event.numtickets,
+            event.location
+        ]
+    )
+    console.log(`added new event: ${res.rows[0].title}`)
+    return res.rows
+}
+
+module.exports = { getAllEvents, getEventById, addEvent }
--- server/routes/events.js
+++ server/routes/events.js
@@ -1,11 +1,23 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-const {getAllEvents}=require('../models/index')
+const { getAllEvents, getEventById, addEvent } = require('../models/index')
 
-router.get("/", async function (req, res, next) {
-  const events = await getAllEvents();
-  res.json({ success: true, payload: events});
-});
+router.get('/', async function (req, res, next) {
+    const events = await getAllEvents()
+    res.json({ success: true, payload: events })
+})
 
-module.exports = router;
+router.get('/:id', async function (req, res, next) {
+    const id = req.params.id
+    const event = await getEventById(id)
+    res.json({ success: true, payload: event })
+})
+
+router.post('/', async function (req, res, next) {
+    const data = req.body
+    const response = await addEvent(data)
+    res.json({ success: true, payload: response })
+})
+
+module.exports = router
