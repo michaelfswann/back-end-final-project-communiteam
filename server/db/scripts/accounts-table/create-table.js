fa9e4b6d5bbb2030f0fc3b9b10818b77c1efe9ae
michaelfswann <mikeswann@mail.com>
2020-12-23 13:07:13 +0000

    create tables

---
+++ server/db/scripts/accounts-table/create-table.js
@@ -0,0 +1,13 @@
+const { query } = require('../../../config/index')
+
+const sqlStatement = `CREATE TABLE IF NOT EXISTS event_table (
+    id SERIAL PRIMARY KEY, title TEXT, date DATE, speaker TEXT, banner TEXT, description TEXT, numtickets INT, location TEXT
+    )`
+
+async function createEventTable() {
+    const res = await query(sqlStatement)
+
+    console.log(`Log: created table called event_table`)
+}
+
+createEventTable()
