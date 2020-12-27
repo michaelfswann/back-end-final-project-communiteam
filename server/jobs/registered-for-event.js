5f61191b2315549095384d49b528f21ed1ac84a7
vmilitaru <vmilitaru28@gmail.com>
2020-12-27 18:21:01 +0000

    fetching data from db using http request

--- server/jobs/registered-for-event.js
+++ server/jobs/registered-for-event.js
@@ -1,15 +1,42 @@
-var schedule = require('node-schedule')
-const { query } = require('../config/index.js')
+//var schedule = require('node-schedule')
+const http = require('http')
 
 // var j = schedule.scheduleJob('42 * * * * *', function () {
 //     console.log('The answer to life, the universe, and everything!')
 // })
 
+
 // we need to get the date from the database on post req for new ticket
+let dataStringEvents = '';
+const req = http.get("http://localhost:3000/events/", function(res) {
+  res.on('data', chunk => {
+    dataStringEvents += chunk;
+  });
+  res.on('end', () => {
+      let allEvents = JSON.parse(dataStringEvents);
+      console.log(allEvents)
+      let eventDate = JSON.parse(dataStringEvents).payload.map((event)=>event.date)
+      console.log(eventDate)
+  });
+});
 
-async function getEventById(id) {
-    const result = await query('SELECT * FROM event_table WHERE id = $1;', [id])
-    return result.rows
-}
+req.on('error', (e) => {
+  console.error(e);
+});
 
-console.log(getEventById(1))
+// we need to get the email address from the database of the attendees
+
+let dataStringEmails = '';
+const request = http.get("http://localhost:3000/tickets/emails/1", function(res) {
+  res.on('data', chunk => {
+    dataStringEmails += chunk;
+  });
+  res.on('end', () => {
+      let allEmails = JSON.parse(dataStringEmails);
+      console.log(allEmails)
+  });
+});
+
+request.on('error', (e) => {
+  console.error(e);
+});
