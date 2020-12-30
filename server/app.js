5e0e97122a58ae7ef97a8c87c0d81c7bcff1c59d
vmilitaru <vmilitaru28@gmail.com>
2020-12-30 12:42:18 +0000

    tickets table amended

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
-app.use(checkJwt)
+//app.use(checkJwt)
 //app.use('/', indexRouter);
 app.use('/protected', checkJwt, eventsRouter)
 app.use('/events', eventsRouter)
--- server/db/scripts/tickets-table/create-table.js
+++ server/db/scripts/tickets-table/create-table.js
@@ -3,9 +3,8 @@ const { query } = require('../../index')
 const sqlStatement = `CREATE TABLE IF NOT EXISTS tickets_table (
     id SERIAL PRIMARY KEY,
     event_id INT NOT NULL,
-    attendee_id TEXT NOT NULL,
-    FOREIGN KEY (event_id) REFERENCES event_table(id) ON UPDATE CASCADE,
-    FOREIGN KEY (attendee_id) REFERENCES accounts_table(uid) ON UPDATE CASCADE
+    attendee_email TEXT NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES event_table(id) ON UPDATE CASCADE
     )`
 
 async function createTicketsTable() {
