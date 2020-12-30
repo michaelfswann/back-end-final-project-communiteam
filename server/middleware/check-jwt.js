891626acf8937b06dde4e036469e2ab74383cb37
michaelfswann <mikeswann@mail.com>
2020-12-30 10:10:24 +0000

    comment format changed

--- server/middleware/check-jwt.js
+++ server/middleware/check-jwt.js
@@ -4,13 +4,13 @@ const jwt = require('express-jwt')
 // const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa')
 
-// Authorization middleware. When used, the
-// Access Token must exist and be verified against
-// the Auth0 JSON Web Key Set
+/* Authorization middleware. When used, the
+Access Token must exist and be verified against
+the Auth0 JSON Web Key Set */
 const checkJwt = jwt({
-    // Dynamically provide a signing key
-    // based on the kid in the header and
-    // the signing keys provided by the JWKS endpoint.
+    /*  Dynamically provide a signing key
+    based on the kid in the header and
+    the signing keys provided by the JWKS endpoint. */
     secret: jwksRsa.expressJwtSecret({
         cache: true,
         rateLimit: true,
