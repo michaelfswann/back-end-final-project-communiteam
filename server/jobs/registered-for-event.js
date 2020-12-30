39866b383f3a5e9e67380d37d94da4768f411d4b
michaelfswann <mikeswann@mail.com>
2020-12-30 10:17:58 +0000

    removed http stuff from registered-for-event job

--- server/jobs/registered-for-event.js
+++ server/jobs/registered-for-event.js
@@ -1,42 +1,5 @@
-//var schedule = require('node-schedule')
-const http = require('http')
+var schedule = require('node-schedule')
 
-// var j = schedule.scheduleJob('42 * * * * *', function () {
-//     console.log('The answer to life, the universe, and everything!')
-// })
-
-
-// we need to get the date from the database on post req for new ticket
-let dataStringEvents = '';
-const req = http.get("http://localhost:3000/events/", function(res) {
-  res.on('data', chunk => {
-    dataStringEvents += chunk;
-  });
-  res.on('end', () => {
-      let allEvents = JSON.parse(dataStringEvents);
-      console.log(allEvents)
-      let eventDate = JSON.parse(dataStringEvents).payload.map((event)=>event.date)
-      console.log(eventDate)
-  });
-});
-
-req.on('error', (e) => {
-  console.error(e);
-});
-
-// we need to get the email address from the database of the attendees
-
-let dataStringEmails = '';
-const request = http.get("http://localhost:3000/tickets/emails/1", function(res) {
-  res.on('data', chunk => {
-    dataStringEmails += chunk;
-  });
-  res.on('end', () => {
-      let allEmails = JSON.parse(dataStringEmails);
-      console.log(allEmails)
-  });
-});
-
-request.on('error', (e) => {
-  console.error(e);
-});
+var j = schedule.scheduleJob('42 * * * * *', function () {
+    console.log('The answer to life, the universe, and everything!')
+})
