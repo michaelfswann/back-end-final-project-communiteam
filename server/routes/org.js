2be11f3f9c84a76d0667286daff6c3b6df699242
vmilitaru <vmilitaru28@gmail.com>
2021-01-06 19:51:30 +0000

    working cloudinary-need to refactor the post request

--- server/routes/org.js
+++ server/routes/org.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router()
-const multer = require('multer')
+const cloudinary = require('cloudinary').v2
 
 const {
     addEvent,
@@ -27,88 +27,40 @@ router.get('/', async function (req, res, next) {
 
 /* EVENT ROUTES */
 
-/* app.post('/api/image-upload', singleUploadCtrl,  async (req, res) => {
-    try {           
-      
-      await cImage.save();
-      return res.json(cImage);
-    } catch(error) {
-      return res.status(422).json({message: error.message});
-    }
-  }); */
-
-
-
-/* const allowedFormats = ['image/jpeg', 'image/png', 'image/jpg']
-const storage = multer.memoryStorage()
-const upload = multer({
-    storage,
-    fileFilter: function (req, file, cb) {
-        if (allowedFormats.includes(file.mimetype)) {
-            cb(null, true)
-        } else {
-            cb(new Error('Not supported file format !'), false)
-        }
-    }
-}) */
-
-const ALLOWED_FORMAT = ['image/jpeg', 'image/png', 'image/jpg'];
-
-const storage = multer.memoryStorage();
-const upload = multer({
-  storage,
-  fileFilter: function(req, file, cb) {
-    if (ALLOWED_FORMAT.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Not supported file format!'), false);
-    }
-  }
-});
-
-/* const singleUpload = upload.single('image');
-
-const singleUploadImgCtrl = (req,res,next) =>{
-    singleUpload(req,res,(error)=>{
-        if (error){
-            return res.status(422).send({message: 'Image upload failed!'});
-        }
-        next();
-    })
-} */
-const singleUpload = upload.single('image');
-
-const singleUploadCtrl = (req, res, next) => {
-  singleUpload(req, res, (error) => {
-    if (error) {
-        return res.status(422).send({message: 'Image upload failed!'});
-    }
-
-    next();
-  })
-}
-
-router.post('/',singleUploadCtrl, function (req, res, next) {
-    try {
-        if (!req.file) {
-            throw new Error('Image is not presented!')}
-            console.log(req.file)
-        /* const file64 = dataUri(req.file);
-        const uploadResult = await cloudinaryUpload(file64.content);
-        const cImage = new CloudinaryImage({cloudinaryId: uploadResult.public_id, url: uploadResult.secure_url});
-
-  --------------------------------------------------------------
+/* router.post('/', function (req, res, next) {
         const data = req.body
         const result = await addEvent(data)
         res.json({ success: true, payload: result }) 
-  -------------------------------------------------------------------      */
-        res.json({ message: 'sent to the server' })
-    } catch (error) {
-        res.status(422).send({ message: error.message })
-    }
-})
-/*--------------------------------------------------------------------------------------------------------------------*/
 
+}) */
+
+/*-------------------------------------------------------Cloudinary config ----------------------*/
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
+
+router.post('/', async function (req, res, next) {
+        const fileStr = req.body.banner
+        const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: 'event_setups'
+        })
+        console.log(uploadedResponse.url)
+       
+
+    /* const data = req.body
+    const result = await addEvent(data)
+    res.json({ success: true, payload: result })  */
+})
+
+/* try { res.json({ msg: 'Victory' })
+} catch (error) {
+    console.log(error)
+    res.status(500).json({ err: 'Something went wrong' })
+} */
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 router.patch('/:id', async function (req, res) {
     const id = req.params.id
