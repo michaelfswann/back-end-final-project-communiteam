94f2052a73efaf046f9833bfaab05d78aba5d16f
michaelfswann <mikeswann@mail.com>
2021-01-12 21:45:15 +0000

    sha-bang added to bash shell

--- server/db/testing.sh
+++ server/db/testing.sh
@@ -1,3 +1,5 @@
+
+#!/bin/bash
 node server/db/scripts/test-db-scripts/test-events-table/test-drop-table.js
 node server/db/scripts/test-db-scripts/test-events-table/test-create-table.js
 node server/db/scripts/test-db-scripts/test-events-table/test-populate-table.js
